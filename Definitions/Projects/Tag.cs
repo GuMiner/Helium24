fa88834a1bbd43b07d6ef37427846ee1c56a02d5
GuMiner <gus.gran@gmail.com>
2018-01-30 18:59:21 -0800

    Adding tags and code to auto-add listed tags.

---
+++ Definitions/Projects/Tag.cs
@@ -0,0 +1,23 @@
+﻿namespace Helium24.Definitions
+{
+    /// <summary>
+    /// Defines a tag for projects
+    /// </summary>
+    public class Tag
+    {
+        public static Tag Hardware { get; } = new Tag("Hardware", "orange");
+        public static Tag Software { get; } = new Tag("Software", "lightblue");
+        public static Tag Laser { get; } = new Tag("Laser", "red");
+        public static Tag Games { get; } = new Tag("Games", "lightgreen");
+
+        public Tag(string name, string htmlColor)
+        {
+            this.Name = name;
+            this.HtmlColor = htmlColor;
+        }
+
+        public string Name { get; }
+
+        public string HtmlColor { get; }
+    }
+}
