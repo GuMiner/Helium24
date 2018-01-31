25028f8820534bc111f86d42530c52827edd58da
GuMiner <gus.gran@gmail.com>
2018-01-30 19:28:27 -0800

    Deduplicating more project code, to simplify creating many more project cards.

---
+++ Models/Projects/Image.cs
@@ -0,0 +1,18 @@
+﻿namespace Helium24.Models
+{
+    /// <summary>
+    /// Defines an image created for use in partial views.
+    /// </summary>
+    public class Image
+    {
+        public Image(string uri, string title)
+        {
+            this.Uri = uri;
+            this.Title = title;
+        }
+
+        public string Uri { get; }
+
+        public string Title { get; }
+    }
+}
