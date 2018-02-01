ea43561852ae257ac4e5f16ed4a06a4d219db245
GuMiner <gus.gran@gmail.com>
2018-01-31 20:25:47 -0800

    Adding the Equation model and EquationCard type and finishing converting Simulator.

---
+++ Models/Projects/Equation.cs
@@ -0,0 +1,18 @@
+﻿namespace Helium24.Models
+{
+    /// <summary>
+    /// Defines an equation created for use in partial views.
+    /// </summary>
+    public class Equation
+    {
+        public Equation(string equationText, string title)
+        {
+            this.EquationText = $"$${equationText}$$";
+            this.Title = title;
+        }
+
+        public string EquationText { get; }
+
+        public string Title { get; }
+    }
+}
