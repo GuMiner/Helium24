e6e5a37d58abe42d192181f98c9beb98b3a49a01
GuMiner <gus.gran@gmail.com>
2018-02-06 08:54:53 -0800

    Adding the ProjectThumbnail type, making nice-looking project thumbnails, starting to add several of the thumbnails, updating TODO list.

---
+++ Models/Projects/ProjectThumbnail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Helium24.Models
+{
+    /// <summary>
+    /// Defines the data necessary to display a thumbnail of a project.
+    /// </summary>
+    public class ProjectThumbnail
+    {
+        public ProjectThumbnail(string thumbnailUri, string projectUri, string title, DateTime date, Tag[] tags)
+        {
+            this.ThumbnailUri = thumbnailUri;
+            this.ProjectUri = projectUri;
+            this.Title = title;
+            this.Date = date;
+            this.Tags = tags;
+        }
+
+        public string ThumbnailUri { get; }
+
+        public string ProjectUri { get; }
+
+        public string Title { get; }
+
+        public DateTime Date { get; }
+
+        public Tag[] Tags { get; }
+    }
+}
