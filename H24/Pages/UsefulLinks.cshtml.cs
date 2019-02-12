4ad226a8d9342c270d17187521fe06421b5338d6
GuMiner <gus.gran@gmail.com>
2019-02-11 17:41:06 -0800

    Adding useful links and cleaning up attributions / index.

---
+++ H24/Pages/UsefulLinks.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace H24.Pages
+{
+    public class UsefulLinksModel : PageModel
+    {
+        public UsefulLinksModel()
+        {
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
