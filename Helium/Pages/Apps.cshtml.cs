7611578c58c89bb2d7ccf6f0a33937dbe26a89c4
GuMiner <gus.gran@gmail.com>
2022-05-15 14:51:40 -0700

    Cleanup dead pages

---
+++ Helium/Pages/Apps.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Helium.Pages
+{
+    public class AppsModel : PageModel
+    {
+        private readonly ILogger<AppsModel> _logger;
+
+        public AppsModel(ILogger<AppsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
