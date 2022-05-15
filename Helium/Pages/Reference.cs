5a4780a31f1a790dde8688ec4ed5db786be5ec66
GuMiner <gus.gran@gmail.com>
2022-05-15 12:25:51 -0700

    Cleanup and organize references in a better view

---
+++ Helium/Pages/Reference.cs
@@ -0,0 +1,33 @@
+﻿namespace Helium.Pages
+{
+    public class ReferenceEntry
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+
+        public string MoreDetails { get; set; }
+
+        public string MoreDetailsLink { get; set; }
+
+        public ReferenceEntry(string name, string link, string moreDetails = "", string moreDetailsLink = "")
+        {
+            this.Name = name;
+            this.Link = link;
+            this.MoreDetails = moreDetails;
+            this.MoreDetailsLink = moreDetailsLink;
+        }
+    }
+
+    public class Reference
+    {
+        public string Title { get; set; }
+
+        public List<ReferenceEntry> Entries { get; set; }
+
+        public Reference(string title, List<ReferenceEntry> entries)
+        {
+            this.Title = title;
+            this.Entries = entries;
+        }
+    }
+}
