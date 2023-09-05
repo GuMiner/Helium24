0c6b81145486da59f4609f4a1189ee47e321f26a
Gus G <gus.gran@gmail.com>
2023-09-05 10:02:04 -0700

    Get the DB setup for air quality metric uploading and retrieval

--- Helium/Controllers/AirQualityController.cs
+++ Helium/Controllers/AirQualityController.cs
@@ -1,12 +1,10 @@
 ﻿using Helium.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Text;
 
 namespace Helium.Controllers
 {
     /// <summary>
-    /// Module for word search results
+    /// Handles saving and returning air quality metrics
     /// </summary>
     [ApiController]
     [Route("api/[controller]/[action]")]
@@ -20,80 +18,40 @@ namespace Helium.Controllers
             this.db = db;
         }
 
-        [AcceptVerbs("GET")]
-        public IActionResult FindMatchingWords([FromQuery] string? search)
+        [HttpPost]
+        public IActionResult Save([FromBody] AirQuality quality, [FromQuery]string sensorId)
         {
-            search = search?.ToUpperInvariant() ?? string.Empty;
+            // TODO make this less public
+            if (sensorId != "1")
+            {
+                return this.BadRequest($"Unknown sensor ID {sensorId}");
+            }
+
+            if (quality.Timestamp == null)
+            {
+                // This is expected to normally be null as this simplifies the sensor upload logic
+                quality.Timestamp = DateTime.UtcNow.ToString("u");
+            }
+
+            this.db.Measurements.Add(quality);
+            this.db.SaveChanges();
 
             return this.Ok(new
             {
-                count = this.GetResultCount(search, GetResultQuery),
-                results = this.GetResults(search, GetResultQuery),
+                count = this.db.Measurements.Count()
             });
         }
 
-        [AcceptVerbs("GET")]
-        public IActionResult FindAnagrams([FromQuery] string? search)
+        [HttpGet]
+        public IActionResult MostRecent([FromQuery]string sensorId, [FromQuery]int count)
         {
-            search = search?.ToUpperInvariant() ?? string.Empty;
-
-            return this.Ok(new
+            // TODO make this less public
+            if (sensorId != "1")
             {
-                count = this.GetResultCount(search, GetAnagramQuery),
-                results = this.GetResults(search, GetAnagramQuery),
-            });
-        }
-
-        private int GetResultCount(string search, Func<string, IQueryable<Word>> queryFunction)
-            => queryFunction(search).Count();
-
-        private List<string> GetResults(string search, Func<string, IQueryable<Word>> queryFunction)
-        {
-            return queryFunction(search)
-                .Select(word => word.WordValue)
-                .OrderBy(word => word)
-                .Take(200)
-                .ToList();
-        }
-
-        private IQueryable<Word> GetResultQuery(string search)
-            => db.Words.FromSqlInterpolated($"SELECT * FROM words WHERE word LIKE {search}");
-
-        private IQueryable<Word> GetAnagramQuery(string search)
-            // Deliberately don't use FromSqlInterpolated as this is raw anagram SQL
-            => db.Words.FromSqlRaw($"SELECT * FROM words WHERE {GetAnagramSearchQuery(search)}");
-
-        private string GetAnagramSearchQuery(string search)
-        {
-            Dictionary<char, int> characters = new();
-            foreach (char character in search)
-            {
-                if (!characters.ContainsKey(character))
-                {
-                    characters.Add(character, 0);
-                }
-
-                characters[character]++;
+                return this.BadRequest($"Unknown sensor ID {sensorId}");
             }
 
-            string comp = "=";
-            if (characters.ContainsKey('_'))
-            {
-                // Greater than or equals, to support anagrams with unknown characters.
-                comp = ">=";
-            }
-
-            StringBuilder searchQuery = new();
-            searchQuery.Append($"(LENGTH(word) = {search.Length})");
-            foreach (KeyValuePair<char, int> keyValuePair in characters)
-            {
-                if (keyValuePair.Key != '_')
-                {
-                    searchQuery.Append($" AND (LENGTH(word) - LENGTH(REPLACE(word, '{keyValuePair.Key}', '')) {comp} {keyValuePair.Value})");
-                }
-            }
-
-            return searchQuery.ToString();
+            return this.Ok(this.db.Measurements.OrderByDescending(m => m.Timestamp).Take(count));
         }
     }
 }
