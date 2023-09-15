27862b569a759d1766f0d53f3c7769e777c3c597
Gustave Granroth <gugra@microsoft.com>
2023-09-15 15:32:39 -0700

    Get basic individual parsing working in zig, with heap-allocated memory

--- DataProcessing/Geneaology/src/main.zig
+++ DataProcessing/Geneaology/src/main.zig
@@ -5,26 +5,50 @@ const test_tree_path = "trees/Granroth-Kooyer Family Tree.ged";
 
 const Gender = enum { M, F, Other };
 
+// GEDCOM-parsed individual
+// Currently ignoring sources, places of residence,
 const Individual = struct {
-    individual_id: []u8, // i.e. @I13142089251@ INDI
-    first_name: []u8, // 1 NAME ... -> 2 GIVN (1)
-    middle_name: []u8, // 1 NAME ... -> 2 GIVN ... (2)
-    last_name: []u8, // 1 NAME ... -> 2 SURN <Name>
+    individual_id: []const u8, // i.e. @I13142089251@ INDI
+    first_name: []const u8, // 1 NAME ... -> 2 GIVN (1)
+    middle_name: []const u8, // 1 NAME ... -> 2 GIVN ... (2)
+    last_name: []const u8, // 1 NAME ... -> 2 SURN <Name>
     gender: Gender, // 1 X
-    group_as_child: []u8, // 1 FAMC <Group>
-    group_as_partner: []u8, // 1 FAMS <Group>
-    birth_date: []u8, // 1 BIRT -> 2 DATE <date>
+    group_as_child: []const u8, // 1 FAMC <Group>
+    group_as_partner: []const u8, // 1 FAMS <Group>
+    birth_date: []const u8, // 1 BIRT -> 2 DATE <date>
+    birth_place: []const u8, // 1 BIRT -> 2 PLAC
+
+    pub fn format(individual: Individual, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        try writer.writeAll("{\n");
+        try writer.print(" individual_id: {s},\n", .{individual.individual_id});
+        try writer.print(" first_name: {s},\n", .{individual.first_name});
+        try writer.print(" last_name: {s},\n", .{individual.last_name});
+        try writer.writeAll("}\n");
+    }
+
+    pub fn init() Individual {
+        return Individual{
+            .individual_id = "",
+            .first_name = "",
+            .middle_name = "",
+            .last_name = "",
+            .gender = Gender.Other,
+            .group_as_child = "",
+            .group_as_partner = "",
+            .birth_date = "",
+            .birth_place = "",
+        };
+    }
 };
 
 // Don't bother reading the files and allocating them -- embed them in the resulting binary for ease-of-use.
 const test_tree = @embedFile(test_tree_path);
-const ParseStates = enum { submitter_record, individual_records };
+const ParseStates = enum { submitter_record, individual_records, done };
 
 pub fn main() !void {
     print("Testing '{s}'\n", .{test_tree_path});
 
-    var line_count: i32 = 0;
-    var lines = std.mem.split(u8, test_tree, "\n");
+    var lines = std.mem.splitScalar(u8, test_tree, '\n');
     var parsing_state = ParseStates.submitter_record;
 
     // skip parsing state
@@ -32,41 +56,55 @@ pub fn main() !void {
         var currentLine = lines.next().?;
         if (std.mem.eql(u8, currentLine, "1 NAME Ancestry.com Member Trees Submitter")) {
             parsing_state = ParseStates.individual_records;
-            return;
         }
     }
 
+    // Only freed at process exit.
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    var individuals = std.ArrayList(Individual).init(arena.allocator());
+
+    var last_line: ?[]const u8 = null;
     while (parsing_state == ParseStates.individual_records) {
+        var individual: Individual = Individual.init();
+
+        if (last_line != null) {
+            // Parse individual ID from here.
+            individual.individual_id = last_line.?;
+        }
+
         // Assume at the start of the next individual record
         while (lines.next()) |line| {
-            _ = line;
+            last_line = line;
+
+            if (std.mem.endsWith(u8, line, "@ INDI")) {
+                if (std.mem.eql(u8, individual.individual_id, "")) {
+                    individual.individual_id = last_line.?;
+                } else {
+                    // Found the next individual, break to parse them.
+                    break;
+                }
+            }
+
+            if (std.mem.startsWith(u8, line, "2 GIVN")) {
+                individual.first_name = line;
+                individual.middle_name = line;
+            }
+
+            if (std.mem.startsWith(u8, line, "2 SURN")) {
+                individual.last_name = line;
+            }
+
+            if (std.mem.eql(u8, line, "0 TRLR")) {
+                parsing_state = ParseStates.done;
+            }
         }
+
+        try individuals.append(individual);
     }
 
-    while (lines.next()) |line| {
-        var parts = std.mem.split(u8, line, " ");
-        line_count = line_count + 1;
-
-        // var part_count: i32 = 0;
-        // while (parts.next()) |path| {
-        //     std.debug.print("-{s}\n", .{path});
-        //     part_count = part_count + 1;
-        // }
-
-        // std.debug.print("--{}\n", .{part_count});
-        // parts.reset();
-
-        // At least one entry
-        var first_entry = parts.next();
-        if (first_entry != null and first_entry.?.len != 0) {
-            // Normal entry, parse
-            print(" {s}\n", .{first_entry.?});
-            var entries: i32 = try std.fmt.parseInt(i32, first_entry.?, 10);
-            print("  {}\n", .{entries});
-        }
-    }
-
-    print("{}\n", .{line_count});
+    print("{any}", .{individuals});
 }
 
 test "simple test" {
